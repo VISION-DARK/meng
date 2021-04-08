27777107f04b60acac48bab1cd71e4edc1fb3d5f
Ramlan404 <79953989+Ramlan404@users.noreply.github.com>
2021-04-08 15:28:06 -0700

    Ramlan ID

--- language/nad.js
+++ language/nad.js
@@ -109,7 +109,6 @@ sisa limit anda : ${isPrem ? '9999' : `${limitCounts}`}
 Upgrade premium bosku, biar bebas gunain bot`
 }
 
-
 exports.uangkau = (pushname, sender, uangkau) => {
 	return`┏━━━━━━━♡ *ATM* ♡━━━━━━━┓
 ┃╭───────────────────
@@ -118,4 +117,40 @@ exports.uangkau = (pushname, sender, uangkau) => {
 ┃│➸ UANG : ${uangkau}
 ┃╰───────────────────
 ┗━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.donasi = () => {
+return`*DONASI KAK*
+Pulsa : 0812-1444-1027
+Gopay : 0855-5924-0360
+Dana : 0855-5924-0360
+Saweria : https://saweria.co/ramlangans
+
+Donasi supaya bot terus update & Aktif`
+}
+
+exports.iklan = (botName, ownerNumbers, ownerName) => {
+return`🔰 -----[ 「 *IKLAN ${botName}* 」 ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 25K/GRUP (BULAN)*
+◯ *BUAT : 100K (BISA JADI OWNER)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *OVO, GOPAY, DANA, PULSA+10K*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *KEUNTUNGAN BUAT BOT :*
+◯ *1. BISA MENJADI OWNER BOT SENDIRI*
+◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+◯ *3. BISA MEMBAWA BOT KE GROUP*
+◯ *4. BISA MENGGUNAKAN COMMAND OWNER*
+◯ *5. BISA MENYEWAKAN BOT KEMBALI*
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/${ownerNumbers}*
+──────────────────────────────
+🔰 -----[「 *POWERED BY ${ownerName}* 」]----- 🔰`
 }
